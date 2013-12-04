e7d9fe42e2f50f9a42413adf2058d45d3302b2da
Felix Sargent <felix.sargent@gmail.com>
2013-12-04 12:22:32 -0800

    Add Shrinkage

---
+++ shrinkage/README.md
@@ -0,0 +1,72 @@
+Todo:
+Accept arguments
+
+1. Specify File
+1. Intergers or Numbers with Decimals
+1. Show boards or not
+1. Specify Shrinkage desired
+
+
+## The Challenge Calculate the green composition for a load of lumber given the
+dried composition for the same load of lumber and a desired shrinkage factor.
+
+## Definitions   Dried Composition = the lengths, thicknesses and count of all
+actual boards in a load   Green Composition = the lengths, thicknesses and count
+of all boards approximated prior to shrinking the load of lumber. Note: this
+composition needs to work mathematically, not necessarily logically as one might
+assume boards shrink :)   Load = a set of data describing a quantity of lumber
+Bundle = a subset of a load   Board Feet = WIDTH/12*INCH*LENGTH*PIECES
+
+
+## Inputs   1. A Load of Dried Composition lumber detailing the following data:
+
+  2. Desired Shrinkage rate expressed as a percentage of total board feet for a
+  load.
+
+  3. Tolerances for results achieved outside of the desired shrinkage rate,
+  likely to be expressed +/- 1% at both the load and bundle level.
+
+## Outputs   1. A Load of Green Composition lumber "bloated" or pre-shrink.
+
+  2. Actual Shrinkage rate for the load and for all bundles within the load.
+
+## Restraints & Liberties   All outputs must be whole numbers.   Not all boards
+must shrink. This is quite obviously, counterintuitive to how lumber must
+actually shrink.   The desired shrinkage rate must be applied to each bundle
+within a load and the desired tolerances must remain true at both the bundle and
+load level.   The steps are written for inches being a constant of 2. Other
+iterations are possible and may be needed to be factored into the steps later.
+(I'm not sure if it is always 2, it just is in this example.
+
+## Steps   1. If any of the INCH entries contain a number other than 2, give an
+error message saying so. If all entries contain 2, move to step 2.
+
+  2. Calculate the board feet for the first bundle (or following bundle, if
+  after the first iteration. in the Dried Composition load (the bundle ID will
+  change when the next bundle starts..
+
+  3. Calculate the desired board feet range for the Green Composition for the
+  first bundle using the Desired Shrinkage Rate and Tolerance at the bundle
+  level.
+
+  4. Make the next to the widest board one foot wider (if after the first
+  iteration, use the previous widest board you are considering.. Note that the
+  total board feet for the bundle will change by a quantity of WIDTH/6 (this is
+  the thing that would change if INCH entries vary from 2..
+
+  5. Calculate the board feet resulting. If the board feet resulting is within
+  the desired range, move onto the next bundle starting at step 2. If the result
+  is lower than the desired range, repeat 4 using another of the longest pieces
+  of lumber. If the result is higher than the desired range, move onto 6.
+
+  6. Add one foot of length to the board that is smaller than the board you were
+  just considering, then move to 5.
+
+  7. When an iteration provides an answer within the desired tolerance, display
+  the iteration's Green Composition along with the actual shrinkage as an output
+  along with the actual shrinkage. If no iterations within a bundle provide an
+  answer within the desired tolerance, display as an output the closest
+  iteration's Green Composition along with the actual shrinkage.
+
+  8. Perform steps 2-7 for each bundle within a load, then display a final total
+  shrinkage and whether or not it meets the desired rate.
---
+++ shrinkage/shrinkage.py
@@ -0,0 +1,122 @@
+from __future__ import division
+import csv
+import pprint
+from collections import defaultdict
+from operator import attrgetter
+from itertools import chain
+from copy import deepcopy
+import argparse
+import sys
+pp = pprint.PrettyPrinter(indent=4)
+
+
+# Shrinkage.py
+# Felix Sargent
+# Computes desired shrinkage from a lumber lot.
+
+parser = argparse.ArgumentParser(description='Calculate Shrinkage')
+parser.add_argument('infile', metavar="file.csv", type=argparse.FileType('U'), 
+                    help='Required csv file')
+parser.add_argument('-b', '--boards', action='store_false', help="Hide board calculations")
+parser.add_argument('-s', '--shrinkage', metavar='%', default=0.10, type=float,
+                    help="Define desired shrinkage (default: .10)")
+# parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
+#                     default=sys.stdout)
+args = parser.parse_args()
+
+class Piece(object):
+    width = 0
+    length = 0
+    inches = 0
+    bundle = 0
+    board_feet = 0
+
+    def __init__(self, width, length, inches, bundle):
+        self.width = width  # in inches
+        self.length = length  # in feet
+        self.inches = inches  # thickness in inches
+        self.bundle = bundle  # bundle number.
+
+    def __repr__(self):
+        return "Length: %s, Width: %s, Board Feet: %s " % (
+            self.length, self.width, self.board_feet()
+        )
+
+    def board_feet(self):
+        return (self.width * self.length * self.inches) / 12
+
+
+class Bundle(object):
+    def __init__(self, bundle_id, pieces):
+        self.bundle_id = bundle_id
+        self.pieces = pieces
+
+    def __repr__(self):
+        return "Bundle ID: %s, Pieces: %s, Board Feet: %s" % (
+            self.bundle_id,
+            len(self.pieces),
+            self.board_feet())
+
+    def board_feet(self):
+        return sum(a.board_feet() for a in self.pieces)
+
+    def sort(self):
+        self.pieces = sorted(self.pieces, key=attrgetter('length', 'width'))
+
+
+def piece_maker(row):
+    pieces = []
+    for _ in range(int(row[3])):
+        pieces.append(
+            Piece(
+                bundle=int(row[0]),
+                inches=int(row[1]),
+                length=int(row[2]),
+                width=int(row[4])
+            )
+        )
+    return pieces
+
+
+def unshrink(dry_bundle):
+    target = dry_bundle.board_feet() * (1 + args.shrinkage)
+    max_length = max(piece.length for piece in dry_bundle.pieces)
+    dry_bundle.sort()
+    # Copy the bundle to a wet one
+    wet_bundle = deepcopy(dry_bundle)
+
+    while wet_bundle.board_feet() < target:
+        if wet_bundle.pieces[0].length < max_length:
+            wet_bundle.pieces[0].length = wet_bundle.pieces[0].length + 1
+        elif wet_bundle.pieces[0].width < 12:  #  No wood is wider than a foot
+            wet_bundle.pieces[0].width = wet_bundle.pieces[0].width + 1
+        else:
+            break
+        wet_bundle.sort()
+
+    pp.pprint("Dry %s" % dry_bundle)
+    if args.boards:
+        pp.pprint(dry_bundle.pieces)
+    pp.pprint("Wet %s" % wet_bundle)
+    if args.boards:
+        pp.pprint(wet_bundle.pieces)
+    print(("Shrinkage: %s%%, Target: %s, distance from target: %s\n" % \
+            (args.shrinkage * 100, target, target - wet_bundle.board_feet())))
+
+def load_data():
+    bundles = []
+    with args.infile as csvfile:
+        shrinkage_table = csv.reader(csvfile, delimiter=',')
+        next(shrinkage_table)
+        pieces = (chain.from_iterable(list(map(piece_maker, shrinkage_table))))
+    bundle_dict = defaultdict(list)
+    for piece in pieces:
+        bundle_dict[piece.bundle].append(piece)
+    bundle_dict = dict(bundle_dict)
+    for k, v in list(bundle_dict.items()):
+        bundles.append(Bundle(k, v))
+    for bundle in bundles:
+        unshrink(bundle)
+
+
+load_data()
